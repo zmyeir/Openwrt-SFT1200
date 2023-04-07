f727dc046854160ff07f20f87ac308b28542fdbb
Mulder Chen <32414985+mulder125@users.noreply.github.com>
2023-04-07 10:28:36 +0800

    test4

--- openwrt-18.06/package/firmware/linux-firmware/qca_ath10k.mk
+++ openwrt-18.06/package/firmware/linux-firmware/qca_ath10k.mk
@@ -1,11 +1,11 @@
-QCA99X0_BOARD_REV:=ddcec9efd245da9365c474f513a855a55f3ac7fe
+QCA99X0_BOARD_REV:=e404444dfc0baf7d0fcde21ab8ec333608c9960c
 QCA99X0_BOARD_FILE:=board-2.bin.$(QCA99X0_BOARD_REV)
 
 define Download/qca99x0-board
-  URL:=https://git.codelinaro.org/clo/qsdk/oss/firmware/ath10k-firmware/-/blob/caf_migration/master/ath10k/QCA99X0/hw2.0/
-  URL_FILE:=board-2.bin?id=$(QCA99X0_BOARD_REV)
+  URL:=https://github.com/kvalo/ath10k-firmware/raw/master/QCA99X0/hw2.0/
+  URL_FILE:=board-2.bin
   FILE:=$(QCA99X0_BOARD_FILE)
-  HASH:=6a8463cfc38ffb732294e0210e39af853c83b912058a64dcbb3f27f605c6a1f9
+  HASH:=f91975dca2435fa6f8570146e6b255c2a70b9ffbdf5ef16a29d67bec7374c11a
 endef
 $(eval $(call Download,qca99x0-board))
 
